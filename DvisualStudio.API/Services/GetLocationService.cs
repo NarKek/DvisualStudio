136628f26a6839ab924c17ab7c32015d165ca02c
illfalcon <kostasminch@gmail.com>
2018-06-12 00:35:18 +0300

    TestingLocation

--- DvisualStudio.API/Services/GetLocationService.cs
+++ DvisualStudio.API/Services/GetLocationService.cs
@@ -15,30 +15,30 @@ namespace DvisualStudio.API.Services
     public class GetLocationService
     {
         private GeoCoordinate _coordinate;
-
-        private void CalculateLocation()
-        {
-            var _watcher = new GeoCoordinateWatcher();
-            _watcher.StatusChanged += (object sender, GeoPositionStatusChangedEventArgs e) =>
-            {
-                switch (e.Status)
-                {
-                    case GeoPositionStatus.Ready:
-                        _coordinate = _watcher.Position.Location;
-                        _watcher.Stop();
-                        break;
-                }
-            };
-            _watcher.Start();
-            while (_coordinate == null)
-            {
-
-            }
-        }
+        //
+        //private void CalculateLocation()
+        //{
+        //    var _watcher = new GeoCoordinateWatcher();
+        //    _watcher.StatusChanged += (object sender, GeoPositionStatusChangedEventArgs e) =>
+        //    {
+        //        switch (e.Status)
+        //        {
+        //            case GeoPositionStatus.Ready:
+        //                _coordinate = _watcher.Position.Location;
+        //                _watcher.Stop();
+        //                break;
+        //        }
+        //    };
+        //    _watcher.Start();
+        //    while (_coordinate == null)
+        //    {
+        //
+        //    }
+        //}
 
         public string GetLocation()
         {
-            CalculateLocation();
+            //CalculateLocation();
             //GeoCoordinate geoCoordinate = new GeoCoordinate();
             //using (GeoCoordinateWatcher watcher = new GeoCoordinateWatcher())
             //{
@@ -53,13 +53,13 @@ namespace DvisualStudio.API.Services
             //some might block tprogrm's request because of the security policy
             //so we decided to hardcore Kirpichnaya 33 <3
 
-            //GeoCoordinate Coordinate = new GeoCoordinate()
-            //{
-            //    Longitude = 55.7783492,
-            //    Latitude = 37.7327935
-            //};
+            _coordinate = new GeoCoordinate()
+            {
+                Longitude = 55.7783492,
+                Latitude = 37.7327935
+            };
 
-            return _coordinate.Latitude.ToString(CultureInfo.InvariantCulture) + "," + _coordinate.Longitude.ToString(CultureInfo.InvariantCulture);
+            return $"{_coordinate.Latitude.ToString(CultureInfo.InvariantCulture)},{_coordinate.Longitude.ToString(CultureInfo.InvariantCulture)}";
         }
     }
 }
